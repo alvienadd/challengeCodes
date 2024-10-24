281c37738d2e29f1de96a0755ebb3cfbd7f5f989
Alvien Laksono <alvien.add@gmail.com>
2024-10-24 15:59:24 +0700

    description 1/366

--- 1 - grow.js
+++ 1 - grow.js
@@ -1,3 +1,6 @@
+//1/366
+//https://www.codewars.com/kata/57f780909f7e8e3183000078/solutions/javascript
+
 function grow(x) {
   const result = x.reduce((acc, curr) => acc * curr, 1);
   return result;
