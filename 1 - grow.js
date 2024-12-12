1788804958731de0e6b81cd0b97bb7b065b0d2c3
alvienadd <alvien.add@gmail.com>
2024-12-12 18:13:16 +0700

    Delete 1 - grow.js

--- 1 - grow.js
+++
@@ -1,9 +0,0 @@
-//1/366
-//https://www.codewars.com/kata/57f780909f7e8e3183000078/solutions/javascript
-
-function grow(x) {
-  const result = x.reduce((acc, curr) => acc * curr, 1);
-  return result;
-}
-
-console.log(grow([2, 2, 2, 2, 2, 2]));
